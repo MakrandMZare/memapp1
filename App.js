d06c30fa0802c7f26fcea3ab8fe94769f7bb4993
MAKRAND MADHUKAR ZARE <66860012+MakrandMZare@users.noreply.github.com>
2022-11-12 12:26:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './assets/images/mlogo.png';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
